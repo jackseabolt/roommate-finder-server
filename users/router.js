71597cbb3ad1bdc7ca153df6373d537d0643c010
John Seabolt <johnseabolt@Johns-MacBook-Pro.local>
2017-12-06 15:57:46 -0800

    validation corrected on post and put endpoints for user

--- users/router.js
+++ users/router.js
@@ -9,7 +9,7 @@ const jsonParser = bodyParser.json();
 
 // ROUTE TO CREATE USERS INITIALLY
 router.post('/', jsonParser, (req, res) => {
-    const requiredFields = ['firstName', 'lastName', 'username', 'password'];
+    const requiredFields = ['username', 'password'];
     const missingField = requiredFields.find(field => !(field in req.body));
 
     if (missingField) {
@@ -21,7 +21,7 @@ router.post('/', jsonParser, (req, res) => {
         });
     }
 
-    const stringFields = ['firstName', 'lastName', 'username', 'password'];
+    const stringFields = ['username', 'password'];
     const nonStringField = stringFields.find(
         field => field in req.body && typeof req.body[field] !== 'string'
     );
@@ -35,7 +35,7 @@ router.post('/', jsonParser, (req, res) => {
         });
     }
 
-    const explicityTrimmedFields = ['firstName', 'lastName', 'username', 'password'];
+    const explicityTrimmedFields = ['username', 'password'];
     const nonTrimmedField = explicityTrimmedFields.find(
         field => req.body[field].trim() !== req.body[field]
     );
@@ -50,8 +50,6 @@ router.post('/', jsonParser, (req, res) => {
     }
 
     const sizedFields = {
-        firstName: { min: 1 },
-        lastName: { min: 1 },
         username: { min: 1 },
         password: { min: 10, max: 72 }
     };
@@ -75,7 +73,7 @@ router.post('/', jsonParser, (req, res) => {
         });
     }
 
-    let { firstName, lastName, username, password } = req.body;
+    let { username, password } = req.body;
     return User.find({ username })
         .count()
         .then(count => {
@@ -90,7 +88,7 @@ router.post('/', jsonParser, (req, res) => {
             return User.hashPassword(password)
         })
         .then(data => {
-            return User.create({ firstName, lastName, username, password: data });
+            return User.create({ username, password: data });
         })
         .then(user => {
             return res.status(201).json(user.apiRepr());
@@ -110,5 +108,53 @@ router.get('/', (req, res) => {
     User.find()
         .then(users => res.json(users.map(user => user.apiRepr())));
 });
+
+
+// ROUTE TO CREATE AND UPDATE USER PROFILE
+router.put('/', jsonParser, (req, res) => {
+    const requiredFields = ['firstName', 'lastName', 'username'];
+    const missingField = requiredFields.find(field => !(field in req.body));
+
+    if (missingField) {
+        return res.status(422).json({
+            code: 422,
+            reason: 'Validation Error',
+            message: 'Missing field',
+            location: missingField
+        });
+    }
+
+    User.findOne({ username: req.body.username })
+        .then(user => {
+            user.firstName = req.body.firstName ? req.body.firstName : null; 
+            user.lastName = req.body.firstName ? req.body.firstName : null; 
+            user.city = req.body.city ? req.body.city : null; 
+            user.state = req.body.state ? req.body.state : null; 
+            user.max_distance = req.body.max_distance ? req.body.max_distance : null; 
+            user.max_price = req.body.max_price ? req.body.max_price: null; 
+            user.pets_have = req.body.pets_have ? req.body.pets_have : null; 
+            user.pets_bothered = req.body.pets_bothered ? req.body.pets_bothered : null; 
+            user.loud_music = req.body.loud_music ? req.body.loud_music : null; 
+            user.loud_music_bothered = req.body.loud_music_bothered ? req.body.loud_music_bothered : null; 
+            user.cigarettes = req.body.cigarettes ? req.body.cigarettes : null; 
+            user.cigarettes_bothered = req.body.cigarettes_bothered ? req.body.cigarettes_bothered : null; 
+            user.drinking_day_per_week = req.body.drinking_day_per_week ? req.body.drinking_day_per_week : null; 
+            user.drinking_bothered = req.body.drinking_bothered ? req.body.drinking_bothered : null; 
+            user.alt_smoking = req.body.alt_smoking ? req.body.alt_smoking : null; 
+            user.alt_smoking_bothered = req.body.alt_smoking_bothered  ? req.body.alt_smoking_bothered  : null; 
+            user.hour_awake = req.body.hour_awake ? req.body.hour_awake : null; 
+            user.hour_asleep = req.body.hour_asleep ? req.body.hour_asleep : null; 
+            user.hours_bothered = req.body.hours_bothered ? req.body.hours_bothered : null; 
+            user.guest_frequency = req.body.guest_frequency ? req.body.guest_frequency : null; 
+            user.guests_bothered = req.body.guests_bothered ? req.body.guests_bothered : null; 
+            user.cleanliness_bothered = req.body.cleanliness_bothered ? req.body.cleanliness_bothered : null; 
+            user.gender = req.body.gender ? req.body.gender : null; 
+            user.gender_bothered = req.body.gender_bothered ? req.body.gender_bothered : null; 
+            user.save(); 
+
+            let updateStatus = req.body.firstName ? 'updated' : 'created'; 
+            return res.status(204).json({ message: `Your profile was ${updateStatus}`, user: user.apiRepr() })
+        }); 
+}); 
 
 module.exports = { router }; 
