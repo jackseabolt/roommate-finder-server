80b8271ed28ffdec07707f6595606fb31d06c97c
ericpcho <ericpcho@gmail.com>
2017-12-06 13:21:02 -0800

    added session time

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
-'use strict'; 
+'use strict';
 
 require('dotenv').load();
 
 module.exports = {
     PORT: process.env.PORT || 8080,
-    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000', 
-    DATABASE_URL: process.env.DATABASE_URL || 'mongodb://localhost/roommate-finder-database', 
-    TEST_DATABASE_URL: 
-        process.env.TEST_DATABASE_URL || 'mongodb://roommate-finder-test-database', 
-    JWT_SECRET: process.env.JWT_SECRET, 
+    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
+    DATABASE_URL: process.env.DATABASE_URL || 'mongodb://localhost/roommate-finder-database',
+    TEST_DATABASE_URL:
+        process.env.TEST_DATABASE_URL || 'mongodb://roommate-finder-test-database',
+    JWT_SECRET: process.env.JWT_SECRET,
     JWT_EXPIRY: process.env.JWT_EXPIRY || '7d'
 }; 
