6d92861a1f85ae7527144a11c1f2cd1864a60013
John Seabolt <johnseabolt@Johns-MacBook-Pro.local>
2017-12-21 17:49:59 -0800

    socket.io removed

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const mongoose = require('mongoose');
 const morgan = require('morgan'); 
 const cors = require('cors'); 
 const passport = require('passport'); 
-const socket = require('socket.io');
-
 const cloudinary = require('cloudinary');
 
 cloudinary.config({ 
@@ -14,29 +12,17 @@ cloudinary.config({
   api_secret: 'xkKjbIvchS4i4dgN57QhG9n8Ky8' 
 });
 
-  
-  
-
-
-
 const { PORT, CLIENT_ORIGIN, DATABASE_URL } = require('./config');
 const { router: usersRouter } = require('./users'); 
 const { router: authRouter, localStrategy, jwtStrategy } = require('./auth');
 const { router: chatRouter } = require('./chat');
 
 const app = express(); 
+
 mongoose.Promise = global.Promise; 
 
 passport.use(localStrategy); 
 passport.use(jwtStrategy); 
-
-// app.use(function(req, res, next) {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Headers', 'X-Requested-With');
-//   res.header('Access-Control-Allow-Headers', 'Content-Type');
-//   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-//   next();
-// });
 
 app.use(
   morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
@@ -48,9 +34,7 @@ app.use(
   cors({
     origin: CLIENT_ORIGIN
   })
- 
 );
-
 
 app.use('/api/users/', usersRouter);
 app.use('/api/auth/', authRouter); 
@@ -69,7 +53,6 @@ function runServer() {
         return reject(err);
       }
       server = app.listen(PORT, () => {
-        connectSocket(server);
         console.log(`The application is listening on port ${PORT}`);
         resolve(); 
       })
@@ -80,27 +63,7 @@ function runServer() {
     }); 
   }); 
 }
-function connectSocket(server){
-  let io = socket(server);
-  console.log('server', server);
-    
-  io.on('connection', (socket) => {
-    console.log(socket.id);
-    //  socket.on('create', function(room){
-    // socket.join('room1');
-    //  });
-    // io.to('room1').emit('room2');
-    // io.in('room3').emit('room4');
-    //changed
-    socket.on('room', function(room){
-      console.log('room found', room);
-      socket.join(room);
-    });
-    socket.on('SEND_MESSAGE', function(data){
-      io.emit('RECEIVE_MESSAGE', data);
-    });
-  });
-}
+
 
 function closeServer() {
   return mongoose.disconnect().then(() => {
